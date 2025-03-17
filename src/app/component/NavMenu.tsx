ced82af9e3ea96b046fe4b55b83dc6319c738734
Enthusi4sm <798092699@qq.com>
2025-03-17 19:53:28 +0800

    前端框架

---
+++ src/app/component/NavMenu.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, {useState} from 'react';
+import {DesktopOutlined, MenuFoldOutlined, MenuUnfoldOutlined, PieChartOutlined,} from '@ant-design/icons';
+import type {MenuProps} from 'antd';
+import {Button, Menu} from 'antd';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+const items: MenuItem[] = [
+    { key: '1', icon: <PieChartOutlined />, label: '最新数据' },
+    { key: '2', icon: <DesktopOutlined />, label: '数据查询' },
+];
+
+const NavMenu: React.FC = () => {
+    const [collapsed, setCollapsed] = useState(false);
+
+    const toggleCollapsed = () => {
+        setCollapsed(!collapsed);
+    };
+
+    return (
+        <div style={{ width: 256 }}>
+            <Button type="primary" onClick={toggleCollapsed} style={{ marginBottom: 16 }}>
+                {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            </Button>
+            <Menu
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                mode="inline"
+                theme="dark"
+                inlineCollapsed={collapsed}
+                items={items}
+            />
+        </div>
+    );
+};
+
+export default NavMenu;
